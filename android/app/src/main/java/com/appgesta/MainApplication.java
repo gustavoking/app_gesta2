7782f79df6b90252d5aff685f5f247b18c5c6f71
Gustavo <gustavorostirollax@gmail.com>
2021-03-23 16:27:58 -0300

    commit here

--- android/app/src/main/java/com/appgesta/MainApplication.java
+++ android/app/src/main/java/com/appgesta/MainApplication.java
@@ -10,9 +10,12 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.rnfs.RNFSPackage; 
+//import com.rnfs.RNFSPackage; 
+
 
 public class MainApplication extends Application implements ReactApplication {
+
+ 
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -27,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new RNFSPackage());
+          //ackages.add(new RNFSPackage());
           return packages;
         }
 
