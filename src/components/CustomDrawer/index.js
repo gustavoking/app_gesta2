9f5e98b66e857ff898096e7d59801ea1d5cdc18a
Gustavo <gustavorostirollax@gmail.com>
2021-03-25 17:33:50 -0300

    last commit

--- src/components/CustomDrawer/index.js
+++ src/components/CustomDrawer/index.js
@@ -1,32 +1,40 @@
-import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
-import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import { AuthContext } from '../../contexts/auth';
+import React, {useContext} from 'react';
+import {View, Text} from 'react-native';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import {AuthContext} from '../../contexts/auth';
 
 export default function CustomDrawer(props) {
+  const {user, signOut} = useContext(AuthContext);
 
-    const { user, signOut } = useContext(AuthContext);
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{alignItems: 'center', justifyContent: 'center', marginTop: 25}}>
+        <Text style={{color: '#FFF', fontSize: 18, marginTop: 5}}>
+          Bem Vindo
+        </Text>
+        <Text
+          style={{
+            color: '#FFF',
+            fontSize: 17,
+            fontWeight: 'bold',
+            paddingBottom: 25,
+          }}>
+          {user && user.nome}
+        </Text>
+      </View>
 
-    return (
-        <DrawerContentScrollView {...props} >
-            <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 25 }}>
-                <Text style={{ color: '#FFF', fontSize: 18, marginTop: 5 }}>
-                    Bem Vindo
-                </Text>
-                <Text style={{ color: '#FFF', fontSize: 17, fontWeight: 'bold', paddingBottom: 25 }}>
-                    {user && user.nome}
-                </Text>
-            </View>
-
-            <DrawerItemList
-                {...props}
-            />
-            <DrawerItem
-                {...props}
-                label="Sair"
-                inactiveBackgroundColor="#3F5C57"
-                onPress={() => signOut()}
-            />
-        </DrawerContentScrollView>
-    );
+      <DrawerItemList {...props} />
+      <DrawerItem
+        {...props}
+        label="Sair"
+        inactiveBackgroundColor="#3F5C57"
+        onPress={() => signOut()}
+      />
+    </DrawerContentScrollView>
+  );
 }
