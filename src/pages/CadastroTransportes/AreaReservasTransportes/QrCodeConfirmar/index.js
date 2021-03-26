940379f0c1543b735ae18789a394ed544791cf2f
Gustavo <gustavorostirollax@gmail.com>
2021-03-25 21:10:33 -0300

    last commit

--- src/pages/CadastroTransportes/AreaReservasTransportes/QrCodeConfirmar/index.js
+++ src/pages/CadastroTransportes/AreaReservasTransportes/QrCodeConfirmar/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useContext} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import Header from '../../../../components/Header';
 import {RNCamera} from 'react-native-camera';
+import firebase from '../../../../services/firebase';
+import {AuthContext} from '../../../../contexts/auth';
+import {format} from 'date-fns';
+import {useEffect} from 'react';
 
-const success = () => {
-  alert('a');
-};
+export default function QrCodeConfirmar({route}) {
+  useEffect(() => {
+    const [diaItem, mesItem, anoItem] = data.dataReserva.split('/');
+    const [horaItem, minutoItem] = data.saidaReserva.split(':');
+    const dateItem = new Date(
+      anoItem,
+      mesItem - 1,
+      diaItem,
+      horaItem - 3,
+      minutoItem,
+    );
 
-export default function QrCodeConfirmar() {
+    const dataMenos3 = new Date();
+    dataMenos3.setHours(dataMenos3.getHours() - 3);
+
+    const dataItemMais5 = dateItem;
+    dataItemMais5.setMinutes(dateItem.getMinutes() + 10);
+
+    const difference = dataMenos3.getTime() > dataItemMais5.getTime(); // This will give difference in milliseconds
+    if (difference) {
+      console.log('passou');
+    } else {
+      console.log('nao passou');
+    }
+    console.log('difference', difference);
+    console.log('dateItem', dateItem);
+  }, []);
+  const {user} = useContext(AuthContext);
+
+  const {data} = route.params;
+
+  const success = () => {};
+
   return (
     <View>
       <Header />
@@ -16,3 +48,16 @@ export default function QrCodeConfirmar() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+});
